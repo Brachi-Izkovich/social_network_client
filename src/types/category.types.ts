eff9034f8967cdf8dfbe5894ba1249aca5f01e34
Brachi <0264brachi@gmail.com>
2025-05-15 16:33:08 +0300

    types and unums

--- src/types/category.types.ts
+++ src/types/category.types.ts
@@ -1,6 +1,8 @@
+import { Collection } from "typescript"
+import { TopicType } from "./topic.types"
+
 export type CategoryType = {
     id: number,
-    name: string,
-    arrImage: string,
-    fileImage: null
+    nameCategory: string,
+    topics: Collection<TopicType> // צריך וירטואל מכיון שלא חייבים לכל קטגוריה נושא
 }
--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,14 +1,10 @@
-
-export enum RoleType {
-    Admin = 'admin',
-    User = 'user'
-}
+import { RoleUserType } from "./enums/roleUserEnum.types";
 
 export type UserType = {
     id: number,
     name: string,
-    role: RoleType,
-    phone: string,
-    email: string,
-    address: string,
+    password: string,
+    imageProfileUrl: string, // לא חובה. איך עושים
+    role: RoleUserType,
+    countMessages: number,
 }
